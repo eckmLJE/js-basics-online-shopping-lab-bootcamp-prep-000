41ea92a30d1ab92299689c9f5af32abf2d42cb83
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-12 13:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function viewCart() {
   } else if (cart.length === 2) {
 		return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + " and " + cart[1].itemName + " at $" + cart[1].itemPrice + ".";
   } else if (cart.length > 2) {
+		var line = "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice;
 		for (var i = 1; i < cart.length - 1; i++) {
-			var line = "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice;
 			line = line + ", " + cart[i].itemName + " at $" + cart[i].itemPrice;
     } return line + " and " + cart[cart.length - 1].itemName + " at $" + cart[cart.length - 1].itemPrice + ".";
   } else {
