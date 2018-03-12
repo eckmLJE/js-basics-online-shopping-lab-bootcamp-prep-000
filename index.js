1ac9804d001c122c531c8270bd26f5880d40ecea
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-12 12:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({
+   itemName: item,
+   itemPrice: Math.floor(Math.random() * 100)
+ })
+ return item + " has been added to your cart."
 }
 
 function viewCart() {
-  // write your code here
+	var cartLength = cart.length;
+  var line = "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice;
+	if (cartLength === 1) {
+		return line + "."
+  } else if (cartLength === 2) {
+		return line + " and " + cart[1].itemName + " at $" + cart[1].itemPrice + ".";
+  } else if (cartLength > 2) {
+		for (var i = 1; i < cartLength - 1; i++) {
+			line = line + ", " + cart[i].itemName + " at $" + cart[i].itemPrice;
+    } return line + ", and " + cart[cartLength - 1].itemName + " at $" + cart[cartLength - 1].itemPrice + ".";
+  } else {
+		return "Your shopping cart is empty.";
+    }
 }
+
+
 
 function total() {
   // write your code here
